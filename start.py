5e4c01d9cdb27142a925c93b8d0e2c4c94c30a4f
“Andrey <andrey.quant@yandex.ru>
2023-05-24 18:19:45 +0600

    feat: rectangular prism (new object)

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+import pygame.gfxdraw
 # Main atributes
 width, heigth = 1280, 720
 
@@ -10,12 +11,30 @@ clock = pygame.time.Clock()
 running = True
 # COLORS
 GREY = (228,228,228)
+BLUE = (103,155,228)
 # lines
+
+# background mesh
 screen.fill("white")
 for x in range(0, width, 40):
     pygame.draw.line(screen, GREY, [x, 0], [x, heigth])
 for y in range(0, heigth, 40):
     pygame.draw.line(screen, GREY, [0, y], [width, y])
+
+# y=kx+b
+def drawLinear(k,b, r):
+    pygame.draw.line(screen, BLUE, [r[0], k*r[0]+b], [r[1], k*r[1]+b]) 
+
+# x=b, vertical line
+def drawVertical(x, r):
+    pygame.draw.line(screen, BLUE, [x, r[0]], [x, r[1]]) 
+
+# Rectangular Prism
+def makeRectPrism(a, b, c):
+    pygame.draw.polygon(screen, BLUE, (a,b,c))
+    pygame.gfxdraw.aapolygon(screen, (a,b,c), BLUE)
+
+makeRectPrism((120,120),(120,240),(240,240))
 
 while running:
     # poll for events
